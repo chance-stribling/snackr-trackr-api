2cb70dc0d8be643bc71a921aafdbaa93bba08b17
chance-stribling <cstribling1717@gmail.com>
2023-01-24 14:46:57 -0500

    Created login, user detail, create and home pages

--- snackr-trackr/src/app/app.module.ts
+++ snackr-trackr/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { AdminCreateComponent } from './admin-create/admin-create.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminCreateComponent,
+    UserDetailsComponent,
+    LoginRegisterComponent
   ],
   imports: [
     BrowserModule,
