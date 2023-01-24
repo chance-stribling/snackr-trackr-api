1e1c3c4c335e96d0d40b5d226f999f22e6f9b4fb
chance-stribling <cstribling1717@gmail.com>
2023-01-24 14:51:38 -0500

    Added pages to routing module

--- snackr-trackr/src/app/app-routing.module.ts
+++ snackr-trackr/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminCreateComponent } from './admin-create/admin-create.component';
+import { HomeComponent } from './home/home.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "/login", component: LoginRegisterComponent},
+  {path: "/create", component: AdminCreateComponent},
+  {path: "/user-details", component: UserDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
