75b9a6b4e1a0ad9d4234b73e297294da01cf1e55
chance-stribling <cstribling1717@gmail.com>
2023-01-25 16:29:41 -0500

    Quick adjustment to the nav menu

--- snackr-trackr/src/app/app-routing.module.ts
+++ snackr-trackr/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminCreateComponent } from './admin-create/admin-create.component';
 import { HomeComponent } from './home/home.component';
 import { LoginRegisterComponent } from './login-register/login-register.component';
+import { SnackSummComponent } from './snack-summ/snack-summ.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginRegisterComponent},
   {path: 'create', component: AdminCreateComponent},
-  {path: 'user-details', component: UserDetailsComponent}
+  {path: 'user-details', component: UserDetailsComponent},
+  {path: 'snack-summary', component: SnackSummComponent}
 ];
 
 @NgModule({
