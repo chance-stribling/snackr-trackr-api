d57a46363e2d2e0f66b2cbf17d8f4e9c8894452c
chance-stribling <cstribling1717@gmail.com>
2023-01-27 13:48:39 -0500

    beginnings of the api, adding user pages

---
+++ snackr-trackr-api/src/main/java/com/stribbles/snackrtrackrapi/SnackrTrackrApiApplication.java
@@ -0,0 +1,13 @@
+package com.stribbles.snackrtrackrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackrTrackrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackrTrackrApiApplication.class, args);
+	}
+
+}
---
+++ snackr-trackr-api/src/test/java/com/stribbles/snackrtrackrapi/SnackrTrackrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stribbles.snackrtrackrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackrTrackrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
